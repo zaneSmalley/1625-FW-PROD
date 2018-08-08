92311ba81b745118ea4a43831371b007a769d551
Zane Smalley <zane.a.smalls@gmail.com>
2018-08-08 17:38:24 -0500

    Updated TalonSRX classes to make them compliant with the 2018 CTRE API changes

---
+++ src/com/team1625/lib/util/drivers/LazyTalonSRX.java
@@ -0,0 +1,29 @@
+package com.team1625.lib.util.drivers;
+
+import com.ctre.CANTalon;
+
+/**
+ * This class is a thin wrapper around the CANTalon that reduces CAN bus / CPU overhead by skipping duplicate set
+ * commands. (By default the Talon flushes the Tx buffer on every set call).
+ */
+public class LazyTalonSRX extends TalonSRX {
+    protected double mLastSet = Double.NaN;
+    protected ControlMode mLastControlMode = null;
+
+    public LazyTalonSRX(int deviceNumber) {
+        super(deviceNumber);
+    }
+
+    public double getLastSet() {
+        return mLastSet;
+    }
+
+    @Override
+    public void set(ControlMode mode, double value) {
+        if (value != mLastSet || mode != mLastControlMode) {
+            mLastSet = value;
+            mLastControlMode = mode;
+            super.set(mode, value);
+        }
+    }
+}
